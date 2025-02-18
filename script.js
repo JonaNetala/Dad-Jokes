eec859ffccd5c0cee5762709ad60b7e262aec79b
JonaNetala <jonathan.netala@gmail.com>
2025-02-18 17:02:00 -0500

    Demo: Replace innerHTML with textContent; Implement evenListener on btn

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ async function generateJoke() {
 
    const res = await fetch('https://icanhazdadjoke.com', config);
    const data = await res.json();
-   jokeEl.innerHTML = data.joke;
+   jokeEl.textContent = data.joke;
 }
 generateJoke();
+
+//Listeners
+jokeBtn.addEventListener('click', generateJoke)
